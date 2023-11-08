05c81297f99629206cb07f8ffe9556788dd2d02d
Alex Castro <alexander72577@gmail.com>
2023-11-08 10:32:05 -0400

    Added 'tutores' table to migrate in DB

---
+++ database/migrations/2023_11_08_142402_create_tutores_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tutores', function (Blueprint $table) {
+            $table->id();
+            $table->string('apellidosTutor');
+            $table->string('nombresTutor');
+            $table->string('titulo');
+            $table->string('genero');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tutores');
+    }
+};
