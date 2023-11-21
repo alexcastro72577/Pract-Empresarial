ea765880ee4a6c4ef4520ed7111b2c706a985f6f
Alex Castro <alexander72577@gmail.com>
2023-11-20 22:58:29 -0400

    Refactored code

--- app/Http/Controllers/PdfCertifEgresoController.php
+++ app/Http/Controllers/PdfCertifEgresoController.php
@@ -13,52 +13,5 @@ class PdfCertifEgresoController extends Controller
     {
         return view('pdfCertifEgreso');
     }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
+   
 }
